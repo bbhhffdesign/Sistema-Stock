690d01fda3371acfc0369f2f86949f4924631eb9
bbhhffdesign <bbhhff.design@gmail.com>
2025-02-19 16:53:19 -0300

    a

---
+++ Sistema_Stock/src/js/distribuidor.js
@@ -0,0 +1,34 @@
+const distribuidores=[
+    {
+        name: "limpieza",
+        items:[
+            {
+                name: "lavandina",
+                price: 1200,
+                quantity: 10,
+                desiredQuantity: 20,
+                isDesiredQuantity: false
+            },
+            {
+                name: "jabon",
+                price: 500,
+                quantity: 0,
+                desiredQuantity: 10,
+                isDesiredQuantity: false
+            },
+            {
+                name: "detergente",
+                price: 800,
+                quantity: 30,
+                desiredQuantity: 20,
+                isDesiredQuantity: true
+            },
+        ]
+    
+    },
+    {
+        name : "alimentos",
+        // aca irian los items de alimentos
+    }
+    // aca irian mas distribuidores
+] 
